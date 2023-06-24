72b2fbb21a483bd0da6671337a82f847d58fe63a
Wanten <WantenMN@gmail.com>
2023-06-24 15:22:03 +0800

    feat(Home): use UserInfo and Collections components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,45 +1,17 @@
-import { fetcherCreateCollection } from "@/apis/collection";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Navigate } from "react-router-dom";
+import UserInfo from "./components/UserInfo";
+import Collections from "./components/Collections";
 
 const Home = () => {
-  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
-    useAuth0();
-
-  const handleClickToken = async () => {
-    const token = await getAccessTokenSilently();
-
-    console.log("Bearer " + token);
-    return token;
-  };
-
-  const handleCreateCollection = async () => {
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await fetcherCreateCollection("test", token);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const { user } = useAuth0();
 
   return (
-    <div>
-      {!isAuthenticated && <Navigate to="/login" />}
-
+    <div className="min-h-screen bg-gradient-to-br from-zinc-100 via-blue-100 to-white">
       {user && (
-        <div>
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-          <button onClick={handleClickToken}>token</button>
-          <br />
-          <button onClick={handleCreateCollection}>create collection</button>
-        </div>
+        <main className="container mx-auto p-4 sm:p-8 ">
+          <UserInfo />
+          <Collections />
+        </main>
       )}
     </div>
   );
