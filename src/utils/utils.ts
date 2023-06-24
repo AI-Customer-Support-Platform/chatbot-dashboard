fe47600487b87873a79756535ef849445c578281
Wanten <WantenMN@gmail.com>
2023-06-24 15:12:56 +0800

    chore(utils): add truncate function

---
+++ src/utils/utils.ts
@@ -0,0 +1,14 @@
+export const truncate = (text: string, maxLength = 40) => {
+  if (text.length <= maxLength) {
+    return text;
+  }
+
+  const truncatedText = text.substring(0, maxLength);
+  const lastSpaceIndex = truncatedText.lastIndexOf(" ");
+
+  if (lastSpaceIndex !== -1) {
+    return truncatedText.substring(0, lastSpaceIndex) + "...";
+  }
+
+  return truncatedText + "...";
+};
