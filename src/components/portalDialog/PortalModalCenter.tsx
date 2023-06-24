1f3aa61ba9d21f379cde6961dabe17534c58705f
Wanten <WantenMN@gmail.com>
2023-06-24 20:19:41 +0800

    chore: add PortalModalCenter component

---
+++ src/components/portalDialog/PortalModalCenter.tsx
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
+import classNames from "classnames";
+
+interface PortalModalCenterProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+  setIsOpen: (show: boolean) => void;
+  isBlur?: boolean;
+}
+
+const PortalModalCenter: React.FC<PortalModalCenterProps> = ({
+  children,
+  isOpen,
+  setIsOpen,
+  isBlur = false,
+}) => {
+  const [visible, setVisible] = useState(false);
+  const mouseDownElement = useRef<EventTarget | null>(null);
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    mouseDownElement.current = event.target;
+  };
+
+  const handleMouseUp = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (event.currentTarget === mouseDownElement.current) setIsOpen(false);
+  };
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        setVisible(true);
+      }, 50);
+    } else {
+      setTimeout(() => {
+        setVisible(false);
+      }, 700);
+    }
+  }, [isOpen]);
+
+  return isOpen
+    ? createPortal(
+        <div
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          className={classNames(
+            "fixed inset-0 flex items-center justify-center bg-black/50 transition-opacity duration-700",
+            {
+              "opacity-0": !visible,
+              "opacity-100": visible,
+              "backdrop-blur-lg": isBlur,
+            }
+          )}
+        >
+          {children}
+        </div>,
+        document.body
+      )
+    : visible
+    ? createPortal(
+        <div
+          className={classNames(
+            "fixed inset-0 flex items-center justify-center bg-black/50 opacity-0 transition-opacity duration-700",
+            {
+              hidden: !visible,
+              "backdrop-blur-lg": isBlur,
+            }
+          )}
+        >
+          {children}
+        </div>,
+        document.body
+      )
+    : null;
+};
+export default PortalModalCenter;
