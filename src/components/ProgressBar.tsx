55e3a7556351592d42f6c132f0e10ebe9503c879
Wanten <WantenMN@gmail.com>
2023-07-22 14:46:40 +0800

    refactor: improve ProgressBar component

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+
 interface ProgressBarProps {
   progressPercentage: number;
 }
@@ -8,10 +10,19 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ progressPercentage }) => {
   return (
     <div className="w-full rounded-full bg-gray-200 dark:bg-gray-700">
       <div
-        className="rounded-full bg-blue-600 p-0.5 text-center text-sm font-bold leading-none text-blue-100"
+        className={classNames(
+          "rounded-full bg-blue-600 p-0.5  text-sm font-bold leading-none text-blue-100",
+          {
+            "text-center": percent > 10,
+          }
+        )}
         style={{ width: `${percent}%` }}
       >
-        {`${formattedPercentage}%`}
+        <span
+          className={classNames({
+            "inline-block translate-x-12 text-black": percent <= 10,
+          })}
+        >{`${formattedPercentage}%`}</span>
       </div>
     </div>
   );
