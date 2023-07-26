12acebe2957bf11df8abfbe5472a7b537679ecdf
Wanten <WantenMN@gmail.com>
2023-07-26 11:01:46 +0800

    feat(FileInputButton): support drag and drop file

--- src/components/buttons/FileInputButton.tsx
+++ src/components/buttons/FileInputButton.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, DragEvent } from "react";
 import UploadFileIcon from "../icons/UploadFileIcon";
 
 interface FileInputButtonProps {
@@ -11,10 +11,40 @@ const FileInputButton: React.FC<FileInputButtonProps> = ({ onChange }) => {
     onChange(file);
   };
 
+  const handleDragOver = (e: DragEvent<HTMLLabelElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.currentTarget.classList.add("border-blue-500", "bg-blue-100");
+  };
+
+  const handleDragLeave = (e: DragEvent<HTMLLabelElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.currentTarget.classList.remove("border-blue-500", "bg-blue-100");
+  };
+
+  const handleDrop = (e: DragEvent<HTMLLabelElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.currentTarget.classList.remove("border-blue-500", "bg-blue-100");
+
+    const file = e.dataTransfer.files?.[0] || null;
+    if (file) {
+      onChange(file);
+    }
+  };
+
   return (
-    <label className="flex w-64 cursor-pointer select-none flex-col items-center rounded-lg border border-blue-500 bg-white px-4 py-6 uppercase tracking-wide text-blue-500 transition duration-200 hover:bg-blue-500 hover:text-white hover:shadow-lg active:bg-blue-600">
+    <label
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+      className="flex w-64 cursor-pointer select-none flex-col items-center rounded-lg border border-blue-500 bg-white px-4 py-6 uppercase tracking-wide text-blue-500 transition duration-200 hover:bg-blue-500 hover:text-white hover:shadow-lg active:bg-blue-600"
+    >
       <UploadFileIcon className="h-20 w-20" />
-      <span className="mt-2 text-base leading-normal">Select a file</span>
+      <span className="mt-2 text-base leading-normal">
+        Select or Drag a file
+      </span>
       <input type="file" className="hidden" onChange={handleFileChange} />
     </label>
   );
