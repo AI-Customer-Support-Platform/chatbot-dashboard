b4be746e734e56d9d78e29a794e8743f996156fa
Wanten <WantenMN@gmail.com>
2023-08-01 20:27:04 +0800

    feat: prevent visiting login page when already logged in

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,25 +1,39 @@
 import svgLogo from "@/assets/imgs/svgs/logo.svg";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { user, loginWithRedirect } = useAuth0();
+  const navigate = useNavigate();
   const handleClickLogin = () => {
     loginWithRedirect();
   };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
   return (
-    <main className="h-screen bg-[#f8f9fa] bg-gradient-to-b from-white to-slate-200">
-      <section className="flex h-full items-center justify-center">
-        <div className="flex flex-col items-center gap-10 sm:flex-row">
-          <img className="w-64" src={svgLogo} />
-          <button
-            onClick={handleClickLogin}
-            className="rounded bg-red-500/80 px-4 py-2 text-lg font-bold text-white hover:scale-105 hover:bg-red-500/90 active:scale-110 "
-          >
-            Login
-          </button>
-        </div>
-      </section>
-    </main>
+    <>
+      {!user && (
+        <main className="h-screen bg-[#f8f9fa] bg-gradient-to-b from-white to-slate-200">
+          <section className="flex h-full items-center justify-center">
+            <div className="flex flex-col items-center gap-10 sm:flex-row">
+              <img className="w-64" src={svgLogo} />
+              <button
+                onClick={handleClickLogin}
+                className="rounded bg-red-500/80 px-4 py-2 text-lg font-bold text-white hover:scale-105 hover:bg-red-500/90 active:scale-110 "
+              >
+                Login
+              </button>
+            </div>
+          </section>
+        </main>
+      )}
+    </>
   );
 };
 export default Login;
