f5b1df39f73251b2fe5ddcf393dac7e13e73e9fa
Wanten <WantenMN@gmail.com>
2023-09-01 11:14:51 +0800

    refactor: extract useUserPlanDetails hook

--- src/pages/Home/APIs/APIs.tsx
+++ src/pages/Home/APIs/APIs.tsx
@@ -1,66 +1,23 @@
-import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import Skeleton from "react-loading-skeleton";
 
 import TitleWithRefreshButton from "@/components/TitleWithRefreshButton";
-import useAPI from "@/hooks/useAPI";
-import { TUserPlanDetail } from "@/types";
 
 import APIItem from "./APIItem";
 import { useAuth0 } from "@auth0/auth0-react";
-import toast from "react-hot-toast";
+import useUserPlanDetails from "@/hooks/useUserPlanDetails";
 
 const APIs = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [initLoaded, setInitLoaded] = useState(false);
-  const [planDetials, setPlanDetails] = useState<TUserPlanDetail>({
-    web: null,
-    instagram: null,
-    line: null,
-  });
-  const { fetcherUserPlanDetail } = useAPI();
   const { t } = useTranslation();
   const { user } = useAuth0();
-
-  const fetchUserPlanDetails = useCallback(async () => {
-    if (isLoading) {
-      return;
-    }
-
-    if (!user?.email_verified) {
-      toast(`⚠️ ${t("Email Verification Required")}`);
-      return;
-    }
-
-    try {
-      setIsLoading(true);
-      setInitLoaded(true);
-      const resp = await fetcherUserPlanDetail();
-      setPlanDetails(resp);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [fetcherUserPlanDetail, isLoading, user, t]);
-
-  useEffect(() => {
-    if (!initLoaded) {
-      const timer = setTimeout(() => {
-        fetchUserPlanDetails();
-      }, 0);
-      return () => {
-        clearTimeout(timer);
-      };
-    }
-  }, [fetchUserPlanDetails, initLoaded]);
+  const { isLoading, refresh, planDetials } = useUserPlanDetails();
 
   return (
     <div className="w-full">
       <TitleWithRefreshButton
         title={t("APIs")}
         isLoading={isLoading}
-        refresh={fetchUserPlanDetails}
+        refresh={refresh}
       />
 
       {!isLoading && user?.email_verified ? (
