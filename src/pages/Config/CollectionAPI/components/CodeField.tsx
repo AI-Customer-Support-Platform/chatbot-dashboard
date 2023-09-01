65b3af632a511593fe8ef53a78b19c5a208c37d3
Wanten <WantenMN@gmail.com>
2023-09-01 11:29:07 +0800

    style: update bg and max-width for CodeField

--- src/pages/Config/CollectionAPI/components/CodeField.tsx
+++ src/pages/Config/CollectionAPI/components/CodeField.tsx
@@ -9,7 +9,7 @@ interface CodeFieldProps {
 const CodeField: React.FC<CodeFieldProps> = ({ collectionId }) => {
   const { t } = useTranslation();
   return (
-    <div className="max-w-2xl rounded-lg bg-white/50 p-4">
+    <div className="max-w-3xl rounded-lg border bg-white p-4">
       <p className="text-lg text-slate-700">
         {t(
           "To integrate our chatbot into your website, kindly copy the following code snippet and paste it within the `<head>` section of your website's index.html file:"
