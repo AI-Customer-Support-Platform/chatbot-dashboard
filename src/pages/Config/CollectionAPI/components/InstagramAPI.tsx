62ed8de515084a4f802eb0de0ab6cd407a4f84d7
Wanten <WantenMN@gmail.com>
2023-09-01 12:08:42 +0800

    feat: add CollectionInfo to LINE,Instagram API pages

--- src/pages/Config/CollectionAPI/components/InstagramAPI.tsx
+++ src/pages/Config/CollectionAPI/components/InstagramAPI.tsx
@@ -2,14 +2,18 @@ import useUserPlanDetails from "@/hooks/useUserPlanDetails";
 import APIItem from "@/pages/Home/APIs/APIItem";
 import { useAuth0 } from "@auth0/auth0-react";
 import Skeleton from "react-loading-skeleton";
+import CollectionInfo from "../../components/CollectionInfo";
+import useCollectionData from "@/hooks/useCollectionData";
 
 const InstagramAPI = () => {
   const { isLoading, planDetials } = useUserPlanDetails();
   const { user } = useAuth0();
+  const { collectionData } = useCollectionData();
   return (
-    <div>
-      <h2>Instagram API</h2>
-      <p className="mb-8">Coming soon</p>
+    <>
+      <CollectionInfo collectionData={collectionData} />
+      <h2 className="mb-4 text-3xl font-bold">Instagram API</h2>
+      <p className="mb-8 text-xl font-bold">Coming soon</p>
       {!isLoading && user?.email_verified ? (
         <section className="w-full">
           <APIItem name="instagram" apiDetails={planDetials.instagram} />
@@ -19,7 +23,7 @@ const InstagramAPI = () => {
           <Skeleton className="h-20 max-w-3xl" />
         </section>
       )}
-    </div>
+    </>
   );
 };
 export default InstagramAPI;
--- src/pages/Config/CollectionAPI/components/LineAPI.tsx
+++ src/pages/Config/CollectionAPI/components/LineAPI.tsx
@@ -2,14 +2,18 @@ import useUserPlanDetails from "@/hooks/useUserPlanDetails";
 import APIItem from "@/pages/Home/APIs/APIItem";
 import { useAuth0 } from "@auth0/auth0-react";
 import Skeleton from "react-loading-skeleton";
+import CollectionInfo from "../../components/CollectionInfo";
+import useCollectionData from "@/hooks/useCollectionData";
 
 const LineAPI = () => {
   const { isLoading, planDetials } = useUserPlanDetails();
   const { user } = useAuth0();
+  const { collectionData } = useCollectionData();
   return (
-    <div>
-      <h2>LINE API</h2>
-      <p className="mb-8">Coming soon</p>
+    <>
+      <CollectionInfo collectionData={collectionData} />
+      <h2 className="mb-4 text-3xl font-bold">LINE API</h2>
+      <p className="mb-8 text-xl font-bold">Coming soon</p>
       {!isLoading && user?.email_verified ? (
         <section className="w-full">
           <APIItem name="line" apiDetails={planDetials.line} />
@@ -19,7 +23,7 @@ const LineAPI = () => {
           <Skeleton className="h-20 max-w-3xl" />
         </section>
       )}
-    </div>
+    </>
   );
 };
 export default LineAPI;
