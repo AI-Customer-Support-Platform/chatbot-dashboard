47e271adce4271d6fe5d7122299872ea28583bf3
Wanten <WantenMN@gmail.com>
2023-09-14 16:25:01 +0800

    chore: hide favicon

--- src/layout/Header/components/Nav/TopNav.tsx
+++ src/layout/Header/components/Nav/TopNav.tsx
@@ -1,13 +1,10 @@
-import svgFavicon from "@/assets/favicon.svg";
 import ConfigButton from "@/components/buttons/ConfigButton";
 import SwitchLangButton from "@/components/buttons/SwitchLangButton";
 
 const TopNav = () => {
   return (
     <nav className="top-0 z-10 flex w-full items-center justify-between gap-5 border-b bg-white px-6 py-3">
-      <section className="flex items-center gap-4">
-        <img className="h-8 w-8" src={svgFavicon} alt="" />
-      </section>
+      <section className="flex items-center gap-4"></section>
       <section className="flex gap-4">
         <ConfigButton />
         <SwitchLangButton />
